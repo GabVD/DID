7ddb54fe9caf8b0acecaee186ddd5c07bc1dfef4
GabVD <gabi01vd@gmail.com>
2023-05-18 21:27:43 -0600

    Dos

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send(
-    '<h1>DID</h1><ul><li><p><b>GET /ver</b></p></li><li><p><b>GET /valor</b></p></li><li><p><b>GET /estado</b></p></li><li><p><b>POST /insertar</b>  => {Ultra1, Ultra2}</p></li><li><p><b>POST /encender</b></p></li><li><p><b>POST /apagar</b></p></li><li><p>/encender</p></li><li><p>/apagar</p></li><li><p>/estado</p></li></ul>')
+    '<h1>DID</h1><ul><li><p><b>GET /ver</b></p></li><li><p><b>GET /valor</b></p></li><li><p><b>GET /estado</b></p></li><li><p><b>POST /insertar</b>  => {Ultra1, Ultra2, fecha}</p></li><li><p><b>POST /encender</b></p></li><li><p><b>POST /apagar</b></p></li><li><p>/encender</p></li><li><p>/apagar</p></li><li><p>/estado</p></li></ul>')
 })
 
 app.get('/ver', (req, res) => {
@@ -78,25 +78,6 @@ app.get('/valor', (req, res) => {
     });
     var wholeData = []
 	db.collection('Valores').limit(1).orderBy('fecha','desc').get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-      
-        wholeData.push(doc.data())
-      });
-      console.log(wholeData)
-      res.send(wholeData)
-    })
-    .catch(error => {
-      console.log('Error!', error);
-  })
-})
-app.get('/grafica', (req, res) => {
-  const db = fire.firestore();
-    db.settings({
-      timestampsInSnapshots: true
-    });
-    var wholeData = []
-	db.collection('Valores').limit(10).orderBy('fecha','desc').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
       
