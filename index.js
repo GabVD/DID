edfd0b494c3d30532201180692ea352a3882f782
GabVD <gabi01vd@gmail.com>
2023-05-20 15:14:49 -0600

    Tres

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send(
-    '<h1>DID</h1><ul><li><p><b>GET /ver</b></p></li><li><p><b>GET /valor</b></p></li><li><p><b>GET /estado</b></p></li><li><p><b>POST /insertar</b>  => {Ultra1, Ultra2, fecha}</p></li><li><p><b>POST /encender</b></p></li><li><p><b>POST /apagar</b></p></li><li><p>/encender</p></li><li><p>/apagar</p></li><li><p>/estado</p></li></ul>')
+    '<h1>             DID</h1><ul><li><p><b>GET /ver</b></p></li><li><p><b>GET /ultimo</b></p></li><li><p><b>GET /estado</b></p></li><li><p><b>POST /insertar</b>  => {Nombre, Ultra1, Ultra2}</p></li><li><p><b>POST /encender</b></p></li><li><p><b>POST /apagar</b></p></li><li><p>/encender</p></li><li><p>/apagar</p></li><li><p>/estado</p></li></ul>')
 })
 
 app.get('/ver', (req, res) => {
@@ -20,7 +20,7 @@ app.get('/ver', (req, res) => {
       timestampsInSnapshots: true
     });
     var wholeData = []
-	db.collection('Valores').orderBy('fecha', 'asc').get()
+	db.collection('/VALORES').orderBy('fecha', 'asc').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
       
@@ -30,7 +30,7 @@ app.get('/ver', (req, res) => {
       res.send(wholeData)
     })
     .catch(error => {
-      console.log('Error!', error);
+      console.log('Hubo un error', error);
   })
 })
 app.get('/estados', (req, res) => {
@@ -49,7 +49,7 @@ app.get('/estados', (req, res) => {
       res.send(wholeData)
     })
     .catch(error => {
-      console.log('Error!', error);
+      console.log('Hubo un error', error);
   })
 })
 app.get('/estado', (req, res) => {
@@ -68,16 +68,16 @@ app.get('/estado', (req, res) => {
       res.send(wholeData)
     })
     .catch(error => {
-      console.log('Error!', error);
+      console.log('Hubo un error', error);
   })
 })
-app.get('/valor', (req, res) => {
+app.get('/ultimo', (req, res) => {
   const db = fire.firestore();
     db.settings({
       timestampsInSnapshots: true
     });
     var wholeData = []
-	db.collection('Valores').limit(1).orderBy('fecha','desc').get()
+	db.collection('/VALORES').limit(1).orderBy('fecha','desc').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
       
@@ -87,7 +87,7 @@ app.get('/valor', (req, res) => {
       res.send(wholeData)
     })
     .catch(error => {
-      console.log('Error!', error);
+      console.log('Hubo un error', error);
   })
 })
 
